ef78d7a0f321bcfb5647e0004aa22180c7885491
Druxys <drudrux@gmail.com>
2021-07-05 15:06:20 +0200

    create entities for messaging feature

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -148,12 +148,24 @@ class User implements UserInterface
      */
     private $competence;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Message::class, mappedBy="created_by")
+     */
+    private $messages;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Canal::class, mappedBy="user")
+     */
+    private $canals;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
         $this->emergencyAperitifs = new ArrayCollection();
         $this->aperitifResponses = new ArrayCollection();
         $this->ideaBoxes = new ArrayCollection();
+        $this->messages = new ArrayCollection();
+        $this->canals = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -477,6 +489,63 @@ class User implements UserInterface
     public function setCompetence(?string $competence): self
     {
         $this->competence = $competence;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Message[]
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setCreatedBy($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getCreatedBy() === $this) {
+                $message->setCreatedBy(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Canal[]
+     */
+    public function getCanals(): Collection
+    {
+        return $this->canals;
+    }
+
+    public function addCanal(Canal $canal): self
+    {
+        if (!$this->canals->contains($canal)) {
+            $this->canals[] = $canal;
+            $canal->addUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCanal(Canal $canal): self
+    {
+        if ($this->canals->removeElement($canal)) {
+            $canal->removeUser($this);
+        }
 
         return $this;
     }
